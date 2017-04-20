a11bcf1c7e34897bca1b59148c4ef4a8326d2381
leesper <pascal7718@gmail.com>
2017-04-20 13:35:25 +0800

    alter query order interface

--- wx/api.go
+++ wx/api.go
@@ -15,16 +15,16 @@ type Payment struct {
 
 type unifiedOrderReq struct {
 	XMLName        xml.Name `xml:"xml"`
-	AppID          string   `xml:"appid"`
-	MchID          string   `xml:"mch_id"`
-	NonceStr       string   `xml:"nonce_str"`
-	Body           string   `xml:"body"`
-	Attach         string   `xml:"attach"`
-	OutTradeNo     string   `xml:"out_trade_no"`
-	TotalFee       string   `xml:"total_fee"`
-	SpbillCreateIP string   `xml:"spbill_create_ip"`
-	NotifyURL      string   `xml:"notify_url"`
-	TradeType      string   `xml:"trade_type"`
+	AppID          string   `xml:"appid"`            // 应用ID
+	MchID          string   `xml:"mch_id"`           // 商户号
+	NonceStr       string   `xml:"nonce_str"`        // 随机字符串
+	Body           string   `xml:"body"`             // 商品描述
+	Attach         string   `xml:"attach"`           // 附加数据
+	OutTradeNo     string   `xml:"out_trade_no"`     // 商户订单号
+	TotalFee       string   `xml:"total_fee"`        // 总金额
+	SpbillCreateIP string   `xml:"spbill_create_ip"` // 终端IP
+	NotifyURL      string   `xml:"notify_url"`       // 通知地址
+	TradeType      string   `xml:"trade_type"`       // 交易类型
 }
 
 func (req unifiedOrderReq) URI() string {
@@ -38,27 +38,27 @@ func (req unifiedOrderReq) SandBoxURI() string {
 // UnifiedOrderRsp is the response returned by /pay/unifiedorder.
 type UnifiedOrderRsp struct {
 	XMLName     xml.Name `xml:"xml"`
-	ReturnCode  string   `xml:"return_code"`
-	ReturnMsg   string   `xml:"return_msg"`
-	AppID       string   `xml:"appid"`
-	MchID       string   `xml:"mch_id"`
-	DeviceInfo  string   `xml:"device_info"`
-	NonceStr    string   `xml:"nonce_str"`
-	Sign        string   `xml:"sign"`
-	ResultCode  string   `xml:"result_code"`
-	ErrCode     string   `xml:"err_code"`
-	ErrCodeDesc string   `xml:"err_code_des"`
-	TradeType   string   `xml:"trade_type"`
-	PrepayID    string   `xml:"prepay_id"`
-	CodeURL     string   `xml:"code_url"`
+	ReturnCode  string   `xml:"return_code"`  // 返回状态码
+	ReturnMsg   string   `xml:"return_msg"`   // 返回信息
+	AppID       string   `xml:"appid"`        // 应用APPID
+	MchID       string   `xml:"mch_id"`       // 商户号
+	DeviceInfo  string   `xml:"device_info"`  // 设备号
+	NonceStr    string   `xml:"nonce_str"`    // 随机字符串
+	Sign        string   `xml:"sign"`         // 签名
+	ResultCode  string   `xml:"result_code"`  // 业务结果
+	ErrCode     string   `xml:"err_code"`     // 错误代码
+	ErrCodeDesc string   `xml:"err_code_des"` // 错误代码描述
+	TradeType   string   `xml:"trade_type"`   // 交易类型
+	PrepayID    string   `xml:"prepay_id"`    // 预支付交易会话标识
 }
 
 type queryOrderReq struct {
 	XMLName       xml.Name `xml:"xml"`
-	AppID         string   `xml:"appid"`
-	MchID         string   `xml:"mch_id"`
-	TransactionID string   `xml:"transaction_id"`
-	NonceStr      string   `xml:"nonce_str"`
+	AppID         string   `xml:"appid"`          // 应用APPID
+	MchID         string   `xml:"mch_id"`         // 商户号
+	TransactionID string   `xml:"transaction_id"` // 微信订单号
+	OutTradeNo    string   `xml:"out_trade_no"`   // 商户订单号
+	NonceStr      string   `xml:"nonce_str"`      // 随机字符串
 }
 
 func (req queryOrderReq) URI() string {
@@ -72,32 +72,32 @@ func (req queryOrderReq) SandBoxURI() string {
 // QueryOrderRsp is the response returned by /pay/orderquery
 type QueryOrderRsp struct {
 	XMLName        xml.Name `xml:"xml"`
-	ReturnCode     string   `xml:"return_code"`
-	ReturnMsg      string   `xml:"return_msg"`
-	AppID          string   `xml:"appid"`
-	MchID          string   `xml:"mch_id"`
-	NonceStr       string   `xml:"nonce_str"`
-	Sign           string   `xml:"sign"`
-	ResultCode     string   `xml:"result_code"`
-	ErrCode        string   `xml:"err_code"`
-	ErrCodeDesc    string   `xml:"err_code_des"`
-	DeviceInfo     string   `xml:"device_info"`
-	OpenID         string   `xml:"open_id"`
-	IsSubscribe    string   `xml:"is_subscribe"`
-	TradeType      string   `xml:"trade_type"`
-	TradeState     string   `xml:"trade_state"`
-	BankType       string   `xml:"bank_type"`
-	TotalFee       string   `xml:"total_fee"`
-	FeeType        string   `xml:"fee_type"`
-	CashFee        string   `xml:"cash_fee"`
-	CashFeeType    string   `xml:"cash_fee_type"`
-	CouponFee      string   `xml:"coupon_fee"`
-	CouponCount    string   `xml:"coupon_count"`
-	TransactionID  string   `xml:"transaction_id"`
-	OutTradeNo     string   `xml:"out_trade_no"`
-	Attach         string   `xml:"attach"`
-	TimeEnd        string   `xml:"time_end"`
-	TradeStateDesc string   `xml:"trade_state_desc"`
+	ReturnCode     string   `xml:"return_code"`      // 返回状态码
+	ReturnMsg      string   `xml:"return_msg"`       // 返回信息
+	AppID          string   `xml:"appid"`            // 应用APPID
+	MchID          string   `xml:"mch_id"`           // 商户号
+	NonceStr       string   `xml:"nonce_str"`        // 随机字符串
+	Sign           string   `xml:"sign"`             // 签名
+	ResultCode     string   `xml:"result_code"`      // 业务结果
+	ErrCode        string   `xml:"err_code"`         // 错误代码
+	ErrCodeDesc    string   `xml:"err_code_des"`     // 错误代码描述
+	DeviceInfo     string   `xml:"device_info"`      // 设备号
+	OpenID         string   `xml:"openid"`           // 用户标识
+	IsSubscribe    string   `xml:"is_subscribe"`     // 是否关注公众账号
+	TradeType      string   `xml:"trade_type"`       // 交易类型
+	TradeState     string   `xml:"trade_state"`      // 交易状态
+	BankType       string   `xml:"bank_type"`        // 付款银行
+	TotalFee       string   `xml:"total_fee"`        // 总金额
+	FeeType        string   `xml:"fee_type"`         // 货币种类
+	CashFee        string   `xml:"cash_fee"`         // 现金支付金额
+	CashFeeType    string   `xml:"cash_fee_type"`    // 现金支付货币类型
+	CouponFee      string   `xml:"coupon_fee"`       // 代金券或立减优惠金额
+	CouponCount    string   `xml:"coupon_count"`     // 代金券或立减优惠使用数量
+	TransactionID  string   `xml:"transaction_id"`   // 微信支付订单号
+	OutTradeNo     string   `xml:"out_trade_no"`     // 商户订单号
+	Attach         string   `xml:"attach"`           // 附加数据
+	TimeEnd        string   `xml:"time_end"`         // 支付完成时间
+	TradeStateDesc string   `xml:"trade_state_desc"` // 交易状态描述
 }
 
 type getSandBoxSignKeyReq struct {
--- wx/client.go
+++ wx/client.go
@@ -125,11 +125,12 @@ func (c *Client) ToPayment(prePayID string) Payment {
 }
 
 // QueryOrder queries order info from Weixin.
-func (c *Client) QueryOrder(transID string) (*QueryOrderRsp, error) {
+func (c *Client) QueryOrder(transID string, tradeNo string) (*QueryOrderRsp, error) {
 	req := queryOrderReq{
 		AppID:         c.config.AppID,
 		MchID:         c.config.MchID,
 		TransactionID: transID,
+		OutTradeNo:    tradeNo,
 		NonceStr:      generateNonceStr(),
 	}
 
@@ -250,30 +251,30 @@ func (c *Client) GetSandBoxSignKey() (*GetSandBoxSignKeyRsp, error) {
 
 // AsyncNotificationResult is the result return from Weixin.
 type AsyncNotificationResult struct {
-	ReturnCode    string `xml:"return_code"`
-	ReturnMsg     string `xml:"return_msg"`
-	AppID         string `xml:"appid"`
-	MchID         string `xml:"mch_id"`
-	DeviceInfo    string `xml:"device_info"`
-	NonceStr      string `xml:"nonce_str"`
-	Sign          string `xml:"sign"`
-	ResultCode    string `xml:"result_code"`
-	ErrCode       string `xml:"err_code"`
-	ErrCodeDesc   string `xml:"err_code_des"`
-	OpenID        string `xml:"openid"`
-	IsSubscribe   string `xml:"is_subscribe"`
-	TradeType     string `xml:"trade_type"`
-	BankType      string `xml:"bank_type"`
-	TotalFee      string `xml:"total_fee"`
-	FeeType       string `xml:"fee_type"`
-	CashFee       string `xml:"cash_fee"`
-	CashFeeType   string `xml:"cash_fee_type"`
-	CouponFee     string `xml:"coupon_fee"`
-	CouponCount   string `xml:"coupon_count"`
-	TransactionID string `xml:"transaction_id"`
-	OutTradeNo    string `xml:"out_trade_no"`
-	Attach        string `xml:"attach"`
-	TimeEnd       string `xml:"time_end"`
+	ReturnCode    string `xml:"return_code"`    // 返回状态码
+	ReturnMsg     string `xml:"return_msg"`     // 返回信息
+	AppID         string `xml:"appid"`          // 应用ID
+	MchID         string `xml:"mch_id"`         // 商户号
+	DeviceInfo    string `xml:"device_info"`    // 设备号
+	NonceStr      string `xml:"nonce_str"`      // 随机字符串
+	Sign          string `xml:"sign"`           // 签名
+	ResultCode    string `xml:"result_code"`    // 业务结果
+	ErrCode       string `xml:"err_code"`       // 错误代码
+	ErrCodeDesc   string `xml:"err_code_des"`   // 错误代码描述
+	OpenID        string `xml:"openid"`         // 用户标识
+	IsSubscribe   string `xml:"is_subscribe"`   // 是否关注公众账号
+	TradeType     string `xml:"trade_type"`     // 交易类型
+	BankType      string `xml:"bank_type"`      // 付款银行
+	TotalFee      string `xml:"total_fee"`      // 总金额
+	FeeType       string `xml:"fee_type"`       // 货币种类
+	CashFee       string `xml:"cash_fee"`       // 现金支付金额
+	CashFeeType   string `xml:"cash_fee_type"`  // 现金支付货币类型
+	CouponFee     string `xml:"coupon_fee"`     // 代金券或立减优惠金额
+	CouponCount   string `xml:"coupon_count"`   // 代金券或立减优惠使用数量
+	TransactionID string `xml:"transaction_id"` // 微信支付订单号
+	OutTradeNo    string `xml:"out_trade_no"`   // 商户订单号
+	Attach        string `xml:"attach"`         // 商家数据包
+	TimeEnd       string `xml:"time_end"`       // 支付完成时间
 }
 
 func (c *Client) doHTTPRequest(uri string, xmlStr string) ([]byte, error) {
